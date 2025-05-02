05ca9c2d5d6a499d2364d7762b2aa8732bf18cc1
chrlz <chrlznft@gmail.com>
2025-05-02 14:06:27 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function backButton() {
+    window.location.href = "index.html";
+}
+
+function goToHome() {
+    window.location.href = "index.html";
+}
+
+function goToTracklist() {
+    window.location.href = "tracklist.html";
+}
+
+function goToGift() {
+    window.location.href = "message.html";
+}
+
